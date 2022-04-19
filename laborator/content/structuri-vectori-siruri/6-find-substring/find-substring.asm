55ed8e2a8f37fe2ce3b3cf6db4590b100fbc97c9
TheFlameStar <anca_negoita@yahoo.com>
2022-04-19 23:08:48 +0300

    bonus-ex 6

--- laborator/content/structuri-vectori-siruri/6-find-substring/find-substring.asm
+++ laborator/content/structuri-vectori-siruri/6-find-substring/find-substring.asm
@@ -12,8 +12,59 @@ global main
 main:
     push ebp
     mov ebp, esp
-
+    mov ecx, source_text
+    mov ebx, substring
+    mov al, byte [ecx]
+    mov dl, byte [ebx]
+    mov esi, 0 ;creste dupa stringul source_text
+    mov edi, 0 ;retine indexul la care se respecta cond
     ; TODO: Print the start indices for all occurrences of the substring in source_text
+compare:
+    cmp dl, 0
+    je exit2
+    mov ebx, substring
+    mov dl, byte [ebx]
+    cmp al, 0
+    je exit1
+    mov al, byte [ecx]
+    mov dl, byte [ebx]
+    cmp al, dl
+    jne continue
 
+index:
+    mov edi, esi
+
+continue:
+    cmp al, dl
+    je increase
+    inc ecx
+    inc esi
+    jmp compare
+
+increase:
+    inc ecx
+    inc ebx
+    inc esi
+    mov al, byte [ecx]
+    mov dl, byte [ebx]
+    cmp al, dl
+    je increase
+    jmp compare
+
+exit1:
+    push -1
+    push print_format
+    call printf
+    add esp, 8
+    jmp exit
+
+exit2:
+    push edi
+    push print_format
+    call printf
+    add esp, 8
+    jmp exit
+
+exit:
     leave
     ret
